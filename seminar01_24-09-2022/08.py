b4fa3fd0cdf5127cc971c51706ebf9cd4a03a3b5
Leonid_Sanov <sanov.87@mail.ru>
2022-09-24 22:45:21 +0500

    homework done

--- seminar01_24-09-2022/08.py
+++ seminar01_24-09-2022/08.py
@@ -4,10 +4,10 @@ x = int(input('Введите координату Х: '))
 y = int(input('Введите координату Y: '))
 
 if x > 0 and y > 0:
-    print('Вторая четверть')
-elif x > 0 and y < 0:
-    print('Третья четверть')
-elif x < 0 and y < 0:
-    print('Четвёртая четверть')
-else:
     print('Первая четверть')
+elif x > 0 and y < 0:
+    print('Вторая четверть')
+elif x < 0 and y < 0:
+    print('Третья четверть')
+else:
+    print('Четвёртая четверть')
--- seminar01_24-09-2022/09.py
+++ seminar01_24-09-2022/09.py
@@ -1,1 +1,13 @@
 # Указав номер четверти прямоугольной системы координат, показать допустимые значения координат для точек этой четверти
+
+quarterNumber = int(input('Введите номер четверти: '))
+if quarterNumber == 1:
+    print('x > 0 and y > 0')
+elif quarterNumber == 2:
+    print('x > 0 and y < 0')
+elif quarterNumber == 3:
+    print('x < 0 and y < 0')
+elif quarterNumber == 4:
+    print('x < 0 and y > 0')
+else:
+    print('Введите целое число от 1 до 4')
